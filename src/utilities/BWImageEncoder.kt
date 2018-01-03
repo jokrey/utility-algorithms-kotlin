60c140a18e42d33447181e1fa7493376e6281874
jokrey <justin.bennett.developer@gmail.com>
2018-01-03 15:46:22 +0100

    Now possible with all width's and heights. However it adds an overhead of at least two bytes.

--- src/utilities/BWImageEncoder.kt
+++ src/utilities/BWImageEncoder.kt
@@ -1,24 +1,62 @@
 package utilities
 
 import utilities.bitsandbytes.BitHelper
-import kotlin.math.log2
-import kotlin.math.sqrt
+import java.util.*
+import kotlin.math.max
+
+fun tests() {
+    println(Arrays.toString(BitHelper.getBits(BitHelper.getMinimalBytes(0))))
+    println(Arrays.toString(BitHelper.getBits(BitHelper.getMinimalBytes(12))))
+    println(Arrays.toString(BitHelper.getBits(BitHelper.getMinimalBytes(128))))
+    println(Arrays.toString(BitHelper.getBits(BitHelper.getMinimalBytes(256))))
+    println(Arrays.toString(BitHelper.getBits(BitHelper.getMinimalBytes(16777215))))
+    println(Arrays.toString(BitHelper.getBits(BitHelper.getMinimalBytes(16777216))))
+    println(Arrays.toString(BitHelper.getBits(BitHelper.getMinimalBytes(4294967295))))
+    println(Arrays.toString(BitHelper.getBits(BitHelper.getMinimalBytes(4294967296))))
+    println(Arrays.toString(BitHelper.getBits(BitHelper.getMinimalBytes(4294967297))))
+
+    println(Arrays.toString(BitHelper.getBits(BitHelper.getMinimalBytes(1))))
+    println(Arrays.toString(BitHelper.getBits(BitHelper.getInNBytes(435, 3))))
+
+    val array = Array(12) {it}
+    println(Arrays.toString(array))
+    var wh_bytecount = 0
+    while (wh_bytecount<5) {
+        wh_bytecount++
+        println(wh_bytecount)
+        println(Arrays.toString(  array.sliceArray(0 until wh_bytecount)  ))
+        println(Arrays.toString(  array.sliceArray(wh_bytecount until 2*wh_bytecount)  ))
+        println()
+    }
+
+    println(BitHelper.getIntFrom(byteArrayOf(0,0,0)))
+    println(BitHelper.getIntFrom(byteArrayOf(0,0,1)))
+    println(BitHelper.getIntFrom(byteArrayOf(0,1,0)))
+    println(BitHelper.getIntFrom(byteArrayOf(0,1,0,0)))
+    println(BitHelper.getIntFrom(byteArrayOf(120,0,0,0,0,0,0,1,0,0)))
+
+    println(Math.ceil((10*10) /8.0).toInt())
+}
+
 
 fun bwimage_encode(raster:Array<Array<Boolean>>): ByteArray {
     val width = raster.size
     val height = raster[0].size
 
-    if(width == 0)
-        throw IllegalArgumentException("raster has to contain data")
-    if(width != height)  //algorithm could handle it without this check, but decoding may be wrong
-        throw IllegalArgumentException("raster width and height have to be equal for now") //TODO for now! Later any width and height are supposed to be accepted
-    if(log2((width*height).toDouble()) % 1 != 0.0)
-        throw IllegalArgumentException("width*height has to be a power of two") //TODO for now! Later any width and height are supposed to be accepted
-    if(width*height < 8)
-        throw IllegalArgumentException("raster has to be at least size 16 for now") //TODO for now! Later any width and height are supposed to be accepted
+    val wh_bytes_count = max(
+            BitHelper.getMinimalBytes(width.toLong()).size,
+            BitHelper.getMinimalBytes(height.toLong()).size)
+    val width_bytes = BitHelper.getInNBytes(width.toLong(), wh_bytes_count)
+    val height_bytes = BitHelper.getInNBytes(height.toLong(), wh_bytes_count)
 
-    var result = ByteArray((width*height) /8)
-    var arr_index = 0
+    val result = ByteArray(Math.ceil((width*height) /8.0).toInt() + width_bytes.size + height_bytes.size)
+
+    for(i in 0 until width_bytes.size)
+        result[i] = width_bytes[i]
+    for(i in 0 until height_bytes.size)
+        result[width_bytes.size + i] = height_bytes[i]
+
+    var arr_index = width_bytes.size + height_bytes.size
     var bit_index = 7
     for (y in 0 until height) {
         for(x in 0 until width) {
@@ -36,18 +74,32 @@ fun bwimage_encode(raster:Array<Array<Boolean>>): ByteArray {
 }
 
 fun bwimage_decode(array: ByteArray):Array<Array<Boolean>> {
-    val stored_bits_count = array.size.toDouble()*8
-    val width = sqrt(stored_bits_count).toInt()
-    val height = sqrt(stored_bits_count).toInt()
+    var wh_bytecount = 0
+    var width = 0
+    var height = 0
+    while (true) {
+        val stored_bits_count = (array.size - 2*wh_bytecount) * 8
+        val new_width = BitHelper.getIntFrom(array.sliceArray(0 until (wh_bytecount+1))).toInt()
+        val new_height = BitHelper.getIntFrom(array.sliceArray((wh_bytecount+1) until 2*(wh_bytecount+1))).toInt()
 
-    if(sqrt(stored_bits_count) % 1 != 0.0)
-         throw IllegalArgumentException("raster width and height have to be equal for now") //TODO for now! Later any width and height are supposed to be accepted
-    if(log2(stored_bits_count) % 1 != 0.0)
-        throw IllegalArgumentException("width*height has to be a power of two") //TODO for now! Later any width and height are supposed to be accepted
+        if((new_width<=0 || new_height<=0) && width!=0 && height!=0 ) break  // indicates a definite wrong read in BitHelper.getIntFrom. Additionally it might confuse the next if clause
 
+        if(width * height <= stored_bits_count && stored_bits_count < new_width*new_height)
+            break
+        else {
+            width=new_width
+            height=new_height
+        }
+        wh_bytecount++
+    }
+
+    return bwimage_decode(width, height, 2*wh_bytecount, array)
+}
+
+private fun bwimage_decode(width:Int, height:Int, start_index:Int, array: ByteArray):Array<Array<Boolean>> {
     val raster = Array(width) {Array(height) {false}}
 
-    var arr_index = 0
+    var arr_index = start_index
     var bit_index = 7
     for (y in 0 until height) {
         for (x in 0 until width) {
